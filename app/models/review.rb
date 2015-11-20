151447af99fb567ea6f2556862e9cfb19f3529e8
boris huruguen <boris.huruguen@gmail.com>
2015-11-20 07:43:05 +0100

    avancement sur le shopping cart (fonctionne à moitié et repositionné)

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
   validates :content,presence: true, length: { minimum: 2 }
   validates :restaurant, presence: true
-  validates :test, presence: true, length: { minimum: 2 }
+  # validates :test, presence: true, length: { minimum: 2 }
   # validates :newprice, presence: true
 end
